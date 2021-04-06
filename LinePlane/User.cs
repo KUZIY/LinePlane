1cdd7eb87b6d880830e3a5006aed245bec48ee5d
KUZIY <180901nikita@gmail.com>
2021-04-07 00:49:41 +0300

    Легкие изменения в классе юзер

--- LinePlane/User.cs
+++ LinePlane/User.cs
@@ -10,7 +10,8 @@ namespace LinePlane
     {
         public int Id { get; set; }
 
-        private string _Login, _Password, _Email;
+        private string _Login,  _Email;
+        private int _Password;
 
         public string Login
         {
@@ -24,7 +25,7 @@ namespace LinePlane
             set { _Email = value; }
         }
 
-        public string Password
+        public int Password
         {
             get { return _Password; }
             set { _Password = value; }
@@ -32,7 +33,7 @@ namespace LinePlane
 
         public User() { }
 
-        public User(string login, string pass, string email)
+        public User(string login, int pass, string email)
         {
             _Login = login;
             _Password = pass;
