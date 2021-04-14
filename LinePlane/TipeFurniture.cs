ce616d0deb81c31984e28c265cda12bb4c0de5eb
KUZIY <180901nikita@gmail.com>
2021-04-14 03:10:58 +0300

    bd
    Создан класс TipeFurniture

---
+++ LinePlane/TipeFurniture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinePlane
+{ 
+class TipeFurniture
+
+{
+    public int Id { get; set; }
+
+        private string _TipeFurniture;
+        public int _IdFurniture;
+
+    public string TipeofFurniture
+        {
+        get { return _TipeFurniture; }
+        set { _TipeFurniture = value; }
+    }
+
+    public int IdFurniture
+        {
+        get { return _IdFurniture; }
+        set { _IdFurniture = value; }
+    }
+
+    public TipeFurniture() { }
+
+        public TipeFurniture(string TipeFurniture, int IdFurniture)
+        {
+            _TipeFurniture = TipeFurniture;
+            _IdFurniture = IdFurniture;
+        }
+
+}
+}
