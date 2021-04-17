a3bc31e788955d3cf62312ed8d6875960aff2779
oglsh <olya_glushkova@bk.ru>
2021-04-17 23:07:59 +0300

    S

--- LinePlaneCore/obj/Debug/net5.0-windows/App.g.cs
+++ LinePlaneCore/obj/Debug/net5.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace LinePlane {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace LinePlane {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
         public static void Main() {
             LinePlane.App app = new LinePlane.App();
             app.InitializeComponent();
--- LinePlaneCore/obj/Debug/net5.0-windows/App.g.i.cs
+++ LinePlaneCore/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace LinePlane {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/LinePlaneCore;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LinePlaneCore;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
