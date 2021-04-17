8f9e565069285170967a1ff04af79ac84002b198
oglsh <olya_glushkova@bk.ru>
2021-04-17 22:47:05 +0300

    Картиночки

---
+++ LinePlaneCore/Draw.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LinePlaneCore
+{
+    internal interface Draw
+    {
+        void Abort(object sender, MouseButtonEventArgs e);
+        void Draw(MouseButtonEventArgs e, System.Windows.Controls.Panel canvas);
+        void Set(MouseEventArgs e, System.Windows.Controls.Panel canvas);
+    }
+
+    internal sealed class Draw_Line : Draw
+    {
+
+        private MainWindow _window;
+
+        private Line line;
+        private bool First_clic = true;
+
+
+        public Draw_Line(MainWindow window)
+        {
+            _window = window;
+        }
+
+
+        public void Abort(object sender, MouseButtonEventArgs e)
+        {
+            if (line != null)
+            {
+                _window.canvas.Children.Remove(line);
+                line = null;
+                First_clic = true;
+              
+            }
+        }
+
+        public void Set(MouseEventArgs e, System.Windows.Controls.Panel canvas)
+        {
+            Point Cursor = _window.Get_Cursor_Point(e);
+
+            if (line == null) return;
+
+            if (First_clic == false)
+            {
+
+                line.X2 = Cursor.X - canvas.Margin.Left;
+                line.Y2 = Cursor.Y - canvas.Margin.Top;
+               
+            }
+            else
+            {
+                int rounding_value = (int)Math.Sqrt(Math.Pow(line.X2 - line.X1, 2) + Math.Pow(line.Y2 - line.Y1, 2)) / 5;
+
+                if (rounding_value > 50)
+                {
+                    rounding_value = 50;
+                }
+
+                if (rounding_value < 4)
+                {
+                    rounding_value = 4;
+                }
+
+
+                if (line.X2 - line.X1 < rounding_value && line.X2 - line.X1 > -rounding_value)
+                {
+                    line.X2 = line.X1;
+                }
+                if (line.Y2 - line.Y1 < rounding_value && line.Y2 - line.Y1 > -rounding_value)
+                {
+                    line.Y2 = line.Y1;
+                }
+
+                line = null;
+
+            }
+        }
+
+        public void Draw(MouseButtonEventArgs e, System.Windows.Controls.Panel canvas)
+        {
+            Point Cursor = _window.Get_Cursor_Point(e);
+
+            if (First_clic)
+            {
+                line = new Line()
+                {
+                    X1 = (line == null) ? Cursor.X - canvas.Margin.Left : line.X2,
+                    Y1 = (line == null) ? Cursor.Y - canvas.Margin.Top : line.Y2,
+                    X2 = Cursor.X - canvas.Margin.Left,
+                    Y2 = Cursor.Y - canvas.Margin.Top
+                };
+
+                line.Stroke = new SolidColorBrush(Colors.Black);
+
+                line.StrokeThickness = 10;
+
+                canvas.Children.Add(line);
+            }
+
+            First_clic = !First_clic;
+        }
+    }
+
+    internal sealed class Draw_Cursor : Draw
+    {
+        public Draw_Cursor(MainWindow window) { }
+
+        public void Abort(object sender, MouseButtonEventArgs e) { }
+        public void Set(MouseEventArgs e, System.Windows.Controls.Panel canvas) { }
+        public void Draw(MouseButtonEventArgs e, System.Windows.Controls.Panel canvas) { }
+    }
+
+    internal class Draw_Square:Draw {
+
+        private MainWindow _window;
+        private double widith;
+        private double height;
+        private Rectangle shape;
+
+
+        public Draw_Square(MainWindow window, int size) {
+
+            _window = window;
+
+        switch (size)
+            {
+                case 1:
+                    {
+                        widith = 100;
+                        height = 100;
+                        break;
+                    }
+
+                case 2:
+                    {
+                        widith = 200;
+                        height = 200;
+                        break;
+                    }
+
+                case 3:
+                    {
+                        widith = 300;
+                        height = 300;
+                        break;
+                    }
+
+            }
+
+            shape = new Rectangle();
+
+            shape.Height = height;
+            shape.Width = widith;
+            var brash = new BrushConverter();
+           
+
+            shape.Fill = (Brush)brash.ConvertFrom("#CC000000");
+
+            window.canvas.Children.Add(shape);
+        }
+
+        public void Abort(object sender, MouseButtonEventArgs e)
+        {
+            _window.canvas.Children.Remove(shape);
+            shape = null;
+        }
+
+        public void Set(MouseEventArgs e, System.Windows.Controls.Panel canvas) {
+
+            Point Cursor = _window.Get_Cursor_Point(e);
+
+
+            if (shape == null) return;
+
+            Canvas.SetLeft (shape, Cursor.X - canvas.Margin.Left - widith/2);
+            Canvas.SetTop (shape, Cursor.Y - canvas.Margin.Top - height/2);
+
+            
+
+        }
+        public void Draw(MouseButtonEventArgs e, System.Windows.Controls.Panel canvas) {
+
+            if (shape != null)
+            shape.Fill = new SolidColorBrush(Colors.Black);
+
+            shape = null;
+        }
+
+
+
+    }
+}
---
+++ LinePlaneCore/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Timers;
+using System.Windows.Markup;
+using System.IO;
+
+namespace LinePlaneCore
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        private Draw a;
+
+        private RegistrationWindow Registration;
+        private EnterWindow Avtoauthorization;
+
+        private readonly List<Line> _lines = new List<Line>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            a = new Draw_Cursor(this);
+        }
+
+        internal Point Get_Cursor_Point(MouseEventArgs e) => Mouse.GetPosition(this);
+
+
+        private void Button_registration_Click(object sender, RoutedEventArgs e)
+        {
+            if (Registration == null)
+            {
+                Registration = new RegistrationWindow();
+            }
+            Registration.Show();
+
+        }
+
+        private void Button_enter_Click(object sender, RoutedEventArgs e)
+        {
+            if (Avtoauthorization == null)
+            {
+                Avtoauthorization = new EnterWindow();
+            }
+
+            Avtoauthorization.Show();
+        }
+
+        #region Прорисовка объектов
+        private void SetLinePosition(MouseEventArgs e)
+        {
+            a.Set(e, canvas);
+
+        }
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            a.Draw(e, canvas);
+
+        }
+
+        private void Abort_Paint(object sender, MouseButtonEventArgs e)
+        {
+            a.Abort(sender, e);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            SetLinePosition(e);
+            
+        }
+
+        #endregion
+
+        private void Cancel_button(object sender, EventArgs e) => Delete_last_canvas_Obj();
+
+        private void User_Button(object sender, MouseEventArgs e)
+        {
+            if (User_Border.Visibility == Visibility.Hidden)
+                User_Border.Visibility = Visibility.Visible;
+            else
+                User_Border.Visibility = Visibility.Hidden;
+        }
+
+
+        private void Cancel(object sender, KeyEventArgs e)
+        {
+            if ((Keyboard.Modifiers & ModifierKeys.Control) > 0)
+            {
+                if (e.Key == Key.Z)
+                {
+                    Delete_last_canvas_Obj();
+                }
+            }
+        }
+
+        private void Delete_last_canvas_Obj()
+        {
+            int cnt = canvas.Children.Count;
+
+            if (cnt > 0)
+                canvas.Children.RemoveAt(cnt - 1);
+        }
+
+        private void ToolBar_Button(object sender, EventArgs e)
+        {
+            DoubleAnimation buttonAnim = new DoubleAnimation();
+
+
+
+
+            if (Tool_grid.Width == 20)
+            {
+                buttonAnim.From = 20;
+                buttonAnim.To = 420;
+                buttonAnim.Duration = TimeSpan.FromSeconds(0.2);
+                Tool_grid.BeginAnimation(Grid.WidthProperty, buttonAnim);
+                buttonAnim.From = 0;
+                buttonAnim.To = 200;
+                ToolBAr_Border.BeginAnimation(Border.WidthProperty, buttonAnim);
+
+            }
+            else
+            {
+                buttonAnim.From = 420;
+                buttonAnim.To = 20;
+                buttonAnim.Duration = TimeSpan.FromSeconds(0.2);
+                Tool_grid.BeginAnimation(Grid.WidthProperty, buttonAnim);
+
+                buttonAnim.From = 200;
+                buttonAnim.To = 0;
+                buttonAnim.Duration = TimeSpan.FromSeconds(0.2);
+                ToolBAr_Border.BeginAnimation(Button.WidthProperty, buttonAnim);
+            }
+
+        }
+
+        private void Button_Save(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveimg = new Microsoft.Win32.SaveFileDialog();
+
+
+
+            saveimg.Filter = "(.PNG)|*.PNG|(.JPEG)|*.JPEG ; *.jpg|(.BMP)|*.bmp" +
+                "|All Files|*.*";
+
+            saveimg.DefaultExt = saveimg.Filter;
+
+            if (saveimg.ShowDialog() == true)
+            {
+                ToImageSource(canvas, saveimg.FileName);
+            }
+        }
+        public static void ToImageSource(Canvas canvas, string filename)
+        {
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)canvas.ActualWidth, (int)canvas.ActualHeight, 96d, 96d, PixelFormats.Pbgra32);
+
+            canvas.Measure(new Size((int)canvas.ActualWidth, (int)canvas.ActualHeight));
+            canvas.Arrange(new Rect(new Size((int)canvas.ActualWidth, (int)canvas.ActualHeight)));
+
+            bmp.Render(canvas);
+
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+
+            using (FileStream file = File.Create(filename))
+            {
+                encoder.Save(file);
+            }
+        }
+        private void Button_NigthDay(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Button_Next(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Cursor(object sender, RoutedEventArgs e)
+        {
+            a = new Draw_Cursor(this);
+            Display_Area.Cursor = Cursors.Arrow;
+        }
+
+        private void Button_Edit(object sender, RoutedEventArgs e)
+        {
+            a = new Draw_Line(this);
+            Display_Area.Cursor = Cursors.Cross;
+        }
+
+        private void Button_Hand(object sender, RoutedEventArgs e)
+        {
+            a = new Draw_Square(this,3);
+            Display_Area.Cursor = Cursors.Hand;
+        }
+    }
+}
